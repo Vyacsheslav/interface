395b36b5e77b3aecec61840d7a46b0052765b47e
Vyacsheslav <129253493+Vyacsheslav@users.noreply.github.com>
2023-10-21 19:14:24 +0500

    Add files via upload

---
+++ five.py
@@ -0,0 +1,19 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from tkinter import *
+>>> def clicked():
+	 lbl.configure(text="Я же просил...")
+
+	 
+>>> window = Tk()
+>>> window.title("Добро пожаловать в приложение PythonRu")
+''
+>>> window.geometry('400x250')
+''
+>>> lbl = Label(window, text="Привет")
+>>> lbl.grid(column=0, row=0)
+>>> txt = Entry(window,width=10)
+>>> txt.grid(column=1, row=0)
+>>> btn = Button(window, text="Не нажимать!", command=clicked)
+>>> btn.grid(column=2, row=0)
+>>> window.mainloop()
