fd2b01f7ff435711c57e8b8078fa0fa9f4bb7210
Vyacsheslav <129253493+Vyacsheslav@users.noreply.github.com>
2023-10-23 13:18:50 +0500

    Update four.py
    New code

--- four.py
+++ four.py
@@ -1,18 +1,14 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> from tkinter import *
->>> def clicked():
+from tkinter import *
+def clicked():
 	lbl.configure(text="Я же просил...")
 
 	
->>> window = Tk()
->>> window.title("Добро пожаловать в приложение PythonRu")
-''
->>> window.geometry('400x250')
-''
->>> lbl = Label(window, text="Привет", font=("Arial Bold", 50))
->>> lbl.grid(column=0, row=0)
->>> btn = Button(window, text="Не нажимать!", command=clicked)
->>> btn.grid(column=1, row=0)
->>> window.mainloop()
->>> 
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+
+window.geometry('400x250')
+lbl = Label(window, text="Привет", font=("Arial Bold", 50))
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Не нажимать!", command=clicked)
+btn.grid(column=1, row=0)
+window.mainloop()
