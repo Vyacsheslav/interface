c0f9eef48c57fa458cb95151f7acaf864ea4ed7d
Vyacsheslav <129253493+Vyacsheslav@users.noreply.github.com>
2023-10-24 11:31:29 +0500

    Update nine.py

--- nine.py
+++ nine.py
@@ -1,10 +1,12 @@
 from tkinter import *
-from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
 window = Tk()
 window.title("Добро пожаловать в приложение PythoneRu")
 window.geometry('400x250')
-chk_state = BooleanVar()
-chk_state.set(True)
-chk = Checkbutton(window, text='Выбрать', var=chk_state)
-chk.grid(column= 0, row= 0)
+rad1 = Radiobutton(window, text='Первый', value=1)
+rad2 = Radiobutton(window, text='Второй', value=2)
+rad3 = Radiobutton(window, text='Третий', value=3)
+rad1.grid(column=0,row=0)
+rad2.grid(column=1,row=0)
+rad3.grid(column=2,row=0)
 window.mainloop()
